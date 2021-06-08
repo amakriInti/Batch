e0dca713e61b53425d9dab60a8201c2dd36d392d
Ali MAKRI <83633193+amakriInti@users.noreply.github.com>
2021-06-08 15:34:15 +0200

    Add files via upload
    manuel

---
+++ DemoBatch.Dto/Personne.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoBatch.Dto
+{
+    [Serializable]
+    public class Personne
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Ville { get; set; }
+        public override string ToString()
+        {
+            return $"{Id}: {Nom} - {Ville}";
+        }
+    }
+}
